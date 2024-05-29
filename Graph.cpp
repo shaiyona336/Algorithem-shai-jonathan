463319eb1a9a810c5583bd7c3af5e1c3ff848d98
shaiyona336 <150026694+shaiyona336@users.noreply.github.com>
2024-05-29 18:42:02 +0300

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -10,32 +10,39 @@ Graph::Graph() {
 
 void Graph::MakeEmptyGraph(int n) {
     adjList.resize(n);
+    for (int i = 0; i < n; i++) {
+        adjList[i] = linkedList();
+    }
 }
 
 
-list<int> Graph::GetAdjList(int u) {
+linkedList Graph::GetAdjList(int u) {
     return adjList[u];
 }
 
 
 bool Graph::IsAdjacent(int u, int v) {
-    for (int adjVertex : adjList[u]) {
-        if (adjVertex == v) {
-            return true;
+    bool flag = false;
+    Node* currentNeightboar = adjList[u].getHead();
+    while (currentNeightboar != NULL) {
+        if (currentNeightboar->data == v) {
+            flag = true;
+            break;
         }
+        currentNeightboar = currentNeightboar->next;
     }
-    return false;
+    return flag;
 }
 
 
 
 void Graph::AddEdge(int u, int v) {
-    if (!IsAdjacent(u,v)) {
-        adjList[u].push_back(v);
+    if (!IsAdjacent(u, v)) {
+        adjList[u].insertAtTail(v);
     }
 }
 
 
-void Graph::RemoveEdge(int u,int v) {
-    adjList[u].remove(v);
+void Graph::RemoveEdge(int u, int v) {
+    adjList[u].deleteNode(v);
 }
